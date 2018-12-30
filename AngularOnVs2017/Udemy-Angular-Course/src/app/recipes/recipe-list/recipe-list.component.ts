9e9424fd0c7d7e59067b1cdcd3443e5c2fe6c8e8
Umberto <Umbertogotti1979@gmail.com>
2018-12-30 14:04:00 +0100

    section 3 completed!

--- AngularOnVs2017/Udemy-Angular-Course/src/app/recipes/recipe-list/recipe-list.component.ts
+++ AngularOnVs2017/Udemy-Angular-Course/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is a simple Test",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQChJQehp7OXvPOzFUeqc4rJjCvl1wtq3Rp8DiJic5-L_dXOTcU"),
+    new Recipe("A Test Recipe2", "This is a simple Test2",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQChJQehp7OXvPOzFUeqc4rJjCvl1wtq3Rp8DiJic5-L_dXOTcU"),
+  ];
+
 
   constructor() { }
 
